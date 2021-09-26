75f65e557b2be80a33883a706a5f11ddfd02e7cf
mszgajewski <mszgajewski@gmail.com>
2021-09-26 20:05:07 +0200

    hyperskill

--- Topics/Functions as objects/Function generator/src/Main.kt
+++ Topics/Functions as objects/Function generator/src/Main.kt
@@ -1,20 +1,14 @@
-fun identity(argument: Int){
-    return argument
-} 
-fun half(argument: Int){
-    return argument / 2
-}
-fun zero(argument: Int){
-    return 0
+fun identity(argument: Int): Int = argument
+fun half(argument: Int): Int = argument / 2
+fun zero(argument: Int): Int = 0
 
 fun generate(functionName: String): (Int) -> Int {
-    if (functionName = "indentity") {
-        return indentity()
-    } else if (functionName = "half") {
-        return half()
-    } else if (functionName = "zero") {
-        return zero()
-    } else {
-        return 0
+    if (functionName == "identity") {
+        return ::identity
+    } else if (functionName == "half") {
+        return ::half
+    } else if (functionName == "zero") {
+        return ::zero
     }
+    return ::zero
 }
