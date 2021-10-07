9086b6fee6c922a6b42e4664aad74c951304065f
mszgajewski <mszgajewski@gmail.com>
2021-10-07 06:44:46 +0200

    hyperskill

---
+++ Topics/Object declarations/Board game basics  Game/src/Main.kt
@@ -0,0 +1,12 @@
+
+object PlayingField {
+    object Size {
+        var width: Int = 2
+        var height: Int = 2
+    }
+
+    fun changeSize(newWidth: Int, newHeight: Int) {
+        var newWidth = Size.width
+        var newHeight = Size.height
+    }
+}
