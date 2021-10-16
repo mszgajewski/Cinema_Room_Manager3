e49ccef8ba834613cf03fd0bb596fef6c19815cc
mszgajewski <mszgajewski@gmail.com>
2021-10-16 12:02:01 +0200

    hyperskill

--- Topics/Enum/Theory/src/Main.kt
+++ Topics/Enum/Theory/src/Main.kt
@@ -4,12 +4,8 @@ import java.io.File
 fun main() {
     val fileName = "MyFile.txt"
     val myFile = File(fileName)
-
     var content = readLine()!!
     content = content.filter { it != '*' }
-
-
-
     myFile.writeText(content)
 
 }
